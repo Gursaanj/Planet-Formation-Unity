e5da0c74a332d877e9bc63dd369317928713f948
Gursaanj <gursaanjresides@gmail.com>
2019-03-29 21:08:25 -0700

    Initial Geomoetry Set
    Created terrain faces as well as a resolution scale to set the number of triangles/vertices on each terrain face for the planet. So by increasing the the resolution, we get more traingles and vertices, giving us more places to place intricate detail

---
+++ Planet Formation/Assets/Scripts/Planet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+
+    // add range
+    [Range(2,256)]
+    public int resolution = 10;
+
+    [SerializeField, HideInInspector]
+    MeshFilter[] meshFilters;
+    TerrainFace[] terrainFaces;
+
+    // To work in on editor
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+    void Initialize()
+    {
+        // Dont want to make 6 sided mesh filter everyime, so only called under special conditions
+
+        if(meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
+
+        // Create a cardinal vector to be set as localUp
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                // Keep the mesh object transform transfixed to the planet's transform 
+                meshObj.transform.parent = transform;
+                // Add a renderer with a default materials shader
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+
+            }
+            //set terrain face for each side of the cube
+            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConsructMesh();
+        }
+    }
+}
---
+++ Planet Formation/Assets/Scripts/TerrainFace.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace {
+    Mesh mesh;
+    int resolution;
+    Vector3 localUp;
+    Vector3 axisA;
+    Vector3 axisB;
+
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.localUp = localUp;
+
+        // axisA is an orientated version of localUp
+
+        axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+
+        // axisB is the perpendicular component between the vertices localUp and axisA
+        
+        axisB = Vector3.Cross(localUp, axisA);
+    }
+
+    public void ConsructMesh()
+    {
+        // Number of vertices that will make up the triangles for the resolution of each TerrainFace
+
+        Vector3[] vertices = new Vector3[resolution * resolution];
+        // Number of trinagles that will form with the decision on resolution (r-1)^2 * 2(triangles per shape) *3(vertices per triangle)
+
+        Vector3[] normals = new Vector3[resolution * resolution];
+
+        int[] triangles = new int[(resolution - 1) * (resolution - 1) * 2* 3];
+        // indexing the triangles generation count
+
+        int triIndex = 0;
+
+        // Decide where the vertex should be on the face, normalized from 0 to 1 (weighted accumulation)
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                // Index where we are on the for loop, where for each iteration over y we do a whole row of x
+
+                int i = x + (y * resolution);
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+
+                // Need to move from the centre of the cube to the appropriate corner on a face by adjusting with axisA and axisB
+
+                Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                //To pop the face of cube onto the sphere
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+                vertices[i] = pointOnUnitSphere;
+                normals[i] = vertices[i].normalized;
+
+                // time to generate the triangles according to the index, in a clockwise format, so that the mesh would orient the right way
+                // the triangles per square will be (i, i+res+1, i+res) and (i, i+1, i+res+1)
+
+                // Making sure to stop counting trinagles at the edges
+
+                if (x != resolution -1 && y != resolution -1)
+                {
+                    // First triangle
+                    triangles[triIndex] = i;
+                    triangles[triIndex + 1] = i + resolution + 1;
+                    triangles[triIndex + 2] = i + resolution;
+                    // Second triangle
+                    triangles[triIndex + 3] = i;
+                    triangles[triIndex + 4] = i + 1;
+                    triangles[triIndex + 5] = i + resolution + 1;
+
+                    triIndex += 6;
+                }
+            }
+        }
+
+        // To make sure that the number of triangles and vertices match up and errors come about
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        //mesh.RecalculateNormals();
+        mesh.normals = normals;
+    }
+}
